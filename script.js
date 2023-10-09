f461a57831198452d937823d9631e3621d97aacb
mr.chalaki <129165551+BABAK-CHALAKI@users.noreply.github.com>
2023-10-09 14:08:01 -0700

    commit

--- script.js
+++ script.js
@@ -13,26 +13,42 @@ function display_all(years){
     let grandchild = document.createElement("P");
     let grandchild_1 = document.createElement("P");
     let grandchild_2 = document.createElement("P");
+    let grandchild_3 = document.createElement("P");
     father.appendChild(child);
     child.appendChild(grandchild);
     child.appendChild(grandchild_1);
     child.appendChild(grandchild_2);
+    child.appendChild(grandchild_3);
     child.classList.add('son');
     grandchild.classList.add('grandchild');
     grandchild_1.classList.add('grandchild_1');
+    grandchild_3.classList.add('grandchild_3');
     grandchild_2.classList.add('grandchild_2');
     child.setAttribute('value',years[i].year);
     grandchild.innerHTML = years[i].title +" "+"_"+" "+ years[i].year
-    grandchild_1.innerHTML = "plot"+":" +" "+ years[i].info.rank
-    grandchild_2.innerHTML = years[i].info.plot
+    
+    if( years[i].info.rating){
+      grandchild_1.innerHTML = "rating"+":" +" "+ years[i].info.rating
+    }  
 
+    grandchild_3.innerHTML = "rank"+":" +" "+ years[i].info.rank
+
+    if(years[i].info.plot){
+    grandchild_2.innerHTML = years[i].info.plot
+    }
+    if(years[i].info.rating > 8){
+      grandchild_1.style.color = "gold"
+    }
+    if(years[i].info.rating <4){
+      grandchild_1.style.color = "red"
+    }
   }
 }
 async function set_data_all(file) {
     let x = await fetch(file);
     let y = await x.text();
     display_all(y);
-    
+    // console.log(y);
   }
   
   function display_years(selected_year){
@@ -62,10 +78,6 @@ async function set_data_all(file) {
 
   set_data_year("https://raw.githubusercontent.com/mehrangholizade/Assignment-18/main/moviedata.json");
   set_data_all("https://raw.githubusercontent.com/mehrangholizade/Assignment-18/main/moviedata.json");
-
-
-
-
 
 
 
